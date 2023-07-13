3e0c51a1dbbb4e0fbd3cf78fe90c17da32298368
Daniela Bento <danibento@overdestiny.com>
2023-07-13 12:14:03 +0100

    Validate also non web transaction
    Signed-off-by: Daniela Bento <danibento@overdestiny.com>

--- lib/lua-nri/newrelic.lua
+++ lib/lua-nri/newrelic.lua
@@ -1159,7 +1159,12 @@ end
 
 -- non web transactions
 local newrelic_start_non_web_transaction = function(application, name)
-  return nr.newrelic_start_non_web_transaction(application, name)
+  local p = ffi.new('newrelic_txn_t*[1]')
+  p[0] = nr.newrelic_start_non_web_transaction(application, name)
+  if p[0] == nil then
+    return nil
+  end
+  return p[0]
 end
 
 local newrelic_end_non_web_transaction = function(transaction_id)
